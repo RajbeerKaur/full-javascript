bd59975197fb5186588ea0fc49f89a144749c1c9
Rajveer <sidrathore663@gmail.com>
2022-06-06 11:09:15 +0300

    second

--- main.js
+++ main.js
@@ -105,3 +105,139 @@ console.log(ourArray );
 var ourArray =[ ["by"] ,["hello" , 2] , [56] ,["by" ,3] ];
 var removeFromArray=ourArray.shift();
 console.log(ourArray );
+
+//unshift()array   add the function in the begging in the array
+var ourArray=["rajbeer","a","b"];
+ourArray.shift();  //ans is ["a","b"];
+ourArray.unshift();// ans is["b"];
+
+//reusable with function
+function reusable(){
+  console.log("hello");
+}
+reusable();
+
+//arguments
+function array(a,c){
+console.log(a-c);
+}
+array(80,20);
+
+//global vs local
+var outWear="shop";  //global
+
+function myWear(){
+  var outWear="shopApple";
+  return outWear;
+}
+  console.log(myWear());
+
+//return value
+var changed =0;
+function change(num){
+  return(num+5) /3;
+}
+changed=change(10);
+console.log(changed);
+
+//if statement
+function trueOrFalse(rajbeer) {
+  if (rajbeer) {
+    return "hi";
+  }
+  return "by";
+}
+console.log(trueOrFalse (true));
+function mySure(kaur) {
+  if (kaur) {
+    return "no";
+  }
+  return "sure";
+}
+console.log(mySure (true));
+
+//equal operater
+function testEqual(val)
+{
+  if (val = 10)
+{
+  return "Equal";
+}
+  return "Not Equal";
+}
+
+console.log(testEqual(15));
+///===
+function compareEqual(a ,b){
+  if (a === b){
+  return "true";
+}
+  return "false";
+}
+
+console.log(compareEqual (10 ,"10"));
+
+//and  or
+
+function test(val){
+  if(val<=50 && val>=20){
+    return "yes";
+  }
+  return "no";
+}
+console.log (test(70));
+
+//else if
+function sumTest(val){
+  if (val<10){
+    return "less";
+  }
+  else if (val > 5) {
+    return "less 5";
+  }
+  else{
+  return "greater";
+  }
+  }
+console.log (sumTest(7));
+
+//object
+var firstName = {      //myname is object
+  "name": "raj",   //name ia a property //raj is a value
+  "age": 34,
+  "friends" : ["everthing"]
+  
+};
+firstName.name ="kaur";
+console.log(firstName.name);
+
+//use new property in a object 
+firstName.class =12;
+
+console.log(firstName.class);
+
+//delete a property from obj
+
+delete myName.age;
+console.log(firstName.age);
+
+//lookup obj
+function phoneLookup (val) {
+  var result = "";
+  var lookup = {
+  "alpha":"adams",
+  "bravo":"boston",
+};
+result =  lookup[val];
+return result;
+}
+console.log(phoneLookup("bravo"));
+
+//while loop
+var myArray =[];
+var i=0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
+console.log(myArray);
